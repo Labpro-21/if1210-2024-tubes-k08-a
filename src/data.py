20ae48d39f40b4950a1792eb98b3a6b45ea6147d
Nadhif Radityo <nadhifradityo@gmail.com>
2024-05-01 12:29:31 +0700

    feat: Modify csv parser to use primordials

--- src/data.py
+++ src/data.py
@@ -1,5 +1,5 @@
+from utils.primordials import *
 import os
-# from utils.primordials import *
 
 root = os.path.dirname(__file__)
 directory = os.path.join(root, '..', 'data')
@@ -9,10 +9,10 @@ def get_arr(filename: str, directory: str) -> list[list[str]]:
     res = []
     
     with open(os.path.join(directory, filename)) as f:
-        f.readline()
-        for line in f:
-            res.append(line.split(";")) # Need to change to the one in utils
-        res.append("__EOP__")
+        lines = string_split(f.read(), "\n")
+        for line in lines:
+            array_push(res, string_split(line, ";"))
+        array_push(res, "__EOP__")
     return res
 
 def load_user() -> tuple[list[int], list[str], list[str], list[str], list[int]]:
@@ -27,11 +27,11 @@ def load_user() -> tuple[list[int], list[str], list[str], list[str], list[int]]:
     temp_user = get_arr("user.csv", directory)
     
     while temp_user[i] != "__EOP__":
-        id_list.append(int(temp_user[i][0]))
-        username_list.append(temp_user[i][1])
-        password_list.append(temp_user[i][2])
-        role_list.append(temp_user[i][3])
-        oc_list.append(int(temp_user[i][4]))
+        array_push(id_list, int(temp_user[i][0]))
+        array_push(username_list, temp_user[i][1])
+        array_push(password_list, temp_user[i][2])
+        array_push(role_list, temp_user[i][3])
+        array_push(oc_list, int(temp_user[i][4]))
         i += 1
     # user_list = [id_list, username_list, password_list, role_list, oc_list]
     return (id_list, username_list, password_list, role_list, oc_list)
@@ -48,11 +48,11 @@ def load_monster() -> tuple[list[int], list[str], list[int], list[int], list[int
     temp_monster = get_arr("monster.csv", directory)
     
     while temp_monster[i] != "__EOP__":
-        id_list.append(int(temp_monster[i][0]))
-        type_list.append(temp_monster[i][1])
-        atk_power_list.append(int(temp_monster[i][2]))
-        def_power_list.append(int(temp_monster[i][3]))
-        hp_list.append(int(temp_monster[i][4]))
+        array_push(id_list, int(temp_monster[i][0]))
+        array_push(type_list, temp_monster[i][1])
+        array_push(atk_power_list, int(temp_monster[i][2]))
+        array_push(def_power_list, int(temp_monster[i][3]))
+        array_push(hp_list, int(temp_monster[i][4]))
         i += 1
     # monster_list = [id_list, type_list, atk_power_list, def_power_list, hp_list]
     return (id_list, type_list, atk_power_list, def_power_list, hp_list)
@@ -67,9 +67,9 @@ def load_item_inventory() -> tuple[list[int], list[str], list[int]]:
     temp_item_inventory = get_arr("item_inventory.csv", directory)
     
     while temp_item_inventory[i] != "__EOP__":
-        user_id_list.append(int(temp_item_inventory[i][0]))
-        type_list.append(temp_item_inventory[i][1])
-        quantity_list.append(int(temp_item_inventory[i][2]))
+        array_push(user_id_list, int(temp_item_inventory[i][0]))
+        array_push(type_list, temp_item_inventory[i][1])
+        array_push(quantity_list, int(temp_item_inventory[i][2]))
         i += 1
     # item_inventory_list = [user_id_list, type_list, quantity_list]
     return (user_id_list, type_list, quantity_list)
@@ -84,9 +84,9 @@ def load_monster_inventory() -> tuple[list[int], list[int], list[int]]:
     temp_monster_inventory = get_arr("monster_inventory.csv", directory)
     
     while temp_monster_inventory[i] != "__EOP__":
-        user_id_list.append(int(temp_monster_inventory[i][0]))
-        monster_id_list.append(int(temp_monster_inventory[i][1]))
-        level_list.append(int(temp_monster_inventory[i][2]))
+        array_push(user_id_list, int(temp_monster_inventory[i][0]))
+        array_push(monster_id_list, int(temp_monster_inventory[i][1]))
+        array_push(level_list, int(temp_monster_inventory[i][2]))
         i += 1
     # monster_inventory_list = [user_id_list, monster_id_list, level_list]
     return (user_id_list, monster_id_list, level_list)
@@ -101,9 +101,9 @@ def load_item_shop() -> tuple[list[str], list[int], list[int]]:
     temp_item_shop = get_arr("item_shop.csv", directory)
     
     while temp_item_shop[i] != "__EOP__":
-        type_list.append(temp_item_shop[i][0])
-        stock_list.append(int(temp_item_shop[i][1]))
-        price_list.append(int(temp_item_shop[i][2]))
+        array_push(type_list, temp_item_shop[i][0])
+        array_push(stock_list, int(temp_item_shop[i][1]))
+        array_push(price_list, int(temp_item_shop[i][2]))
         i += 1
     # item_shop_list = [type_list, stock_list, price_list]
     return (type_list, stock_list, price_list)
@@ -118,9 +118,9 @@ def load_monster_shop() -> tuple[list[int], list[int], list[int]]:
     temp_monster_shop = get_arr("monster_shop.csv", directory)
     
     while temp_monster_shop[i] != "__EOP__":
-        monster_id_list.append(int(temp_monster_shop[i][0]))
-        stock_list.append(int(temp_monster_shop[i][1]))
-        price_list.append(int(temp_monster_shop[i][2]))
+        array_push(monster_id_list, int(temp_monster_shop[i][0]))
+        array_push(stock_list, int(temp_monster_shop[i][1]))
+        array_push(price_list, int(temp_monster_shop[i][2]))
         i += 1
     # monster_shop_list = [monster_id_list, stock_list, price_list]
     return (monster_id_list, stock_list, price_list)
