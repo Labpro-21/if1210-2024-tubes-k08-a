6029ccdfc915bc43dd2e5a5a29cbb5b9f972c451
Nadhif Radityo <nadhifradityo@gmail.com>
2024-05-12 21:34:06 +0700

    feat(utils): Move csv parser from data.py to its own module and implemented mixin utils

---
+++ src/utils/mixin/__init__.py
@@ -0,0 +1,6 @@
+from .mixin import _mixin_new, _mixin_is_override, _mixin_call_override, _mixin_call_super
+
+mixin_new = _mixin_new
+mixin_is_override = _mixin_is_override
+mixin_call_override = _mixin_call_override
+mixin_call_super = _mixin_call_super
