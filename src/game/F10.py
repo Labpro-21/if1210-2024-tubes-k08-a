3b40da408734023d4693a4fa0ef540d1cb10479e
Julbe26 <16523178@std.stei.itb.ac.id>
2024-05-19 15:42:11 +0700

    F10 baru, feat (game) Implemented

--- src/game/F10.py
+++ src/game/F10.py
@@ -47,28 +47,28 @@ def beli_mons():
         id = mons_shop[i][0]
         type = mons[i][1]
         type_inv_id = mons_inv[i][1]
-        atk = mons[i][2]
-        defence = mons [i][3]
-        hp = mons [i][4]
         stok = mons_shop [i][1]
         price = mons_shop [i][2]
         oc = baca_oc[pilih_id][4]
         if aksi_beli_monster == id:
             oc = int(oc)
             price = int(price)
+            stok = int(stok)
             if aksi_beli_monster == type_inv_id:
                 print("Monster sudah ada di inventory kamu :), transaksi dibatalkan")
             else:
                 if stok >= 1:
                     if oc >= price:
-                        print(f"kamu berhasil membeli {type}")
-                        print(f"Sisa O.W.C.A kamu {oc-price}")
+                        print(f"kamu berhasil membeli {type}, dan {type} telah disimpan di inventory.")
+                        print(f"Sisa O.C kamu {oc-price}")
                         oc = oc - price
+                        oc = str(oc)
                         baca_oc[pilih_id][4] = oc
                         stok -= 1
+                        stok = str(stok)
                         mons_shop [i][1] = stok
                     elif oc < price:
-                        print("O.W.C.A kamu kurang :(")
+                        print("O.C kamu kurang :(")
                 else:
                     print("Stok monster habis")
 def beli_potion():
@@ -77,19 +77,41 @@ def beli_potion():
         type_1 = item_Shop[j][0]
         stok_1 = item_Shop[j][1]
         harga_1 = item_Shop[j][2]
-    if aksi_beli_potion == id_1:
-        oc = int(oc)
-        harga_1 = int(harga_1)
-        jumlah = jumlah_beli_potion * harga_1
-        if oc >= harga_1:
-            print(f"kamu berhasil membeli {type_1}")
-            print(f"Sisa O.W.C.A kamu {oc-jumlah}")
-            oc = oc - jumlah
-            baca_oc[pilih_id][4] = oc
-        elif oc < harga_1:
-            print("O.W.C.A kamu kurang :(")
+        oc = baca_oc[pilih_id][4]
+        if aksi_beli_potion == id_1:
+            oc = int(oc)
+            harga_1 = int(harga_1)
+            stok_1 = int(stok_1)
+            jumlah = int(jumlah_beli_potion * harga_1)
+            if stok_1 >= 1:
+                if oc >= harga_1:
+                    print(f"kamu berhasil membeli {type_1}")
+                    print(f"Sisa O.C kamu {oc-jumlah}, sebanyak {jumlah_beli_potion}")
+                    oc = oc - jumlah
+                    oc = str(oc)
+                    baca_oc[pilih_id][4] = oc
+                    stok_1 -= 1
+                    stok_1 = str(stok_1)
+                    item_Shop[j][1] = stok_1
+                elif oc == harga_1:
+                    beli_ga = str(input("O.C kamu pas-pasan untuk beli potion... mau lanjut beli? (Y/N)"))
+                    if beli_ga == "Y" or beli_ga == "y":
+                        print(f"kamu berhasil membeli {type_1}")
+                        print(f"Sisa O.C kamu {oc-jumlah}, sebanyak {jumlah_beli_potion}")
+                        oc = oc - jumlah
+                        oc = str(oc)
+                        baca_oc[pilih_id][4] = oc
+                        stok_1 -= 1
+                        stok_1 = str(stok_1)
+                        item_Shop[j][1] = stok_1
+                    elif beli_ga =="N" or beli_ga =="n":
+                        print("Okay transaksi dibatalkan, selamat menabung!!")
+                elif oc < harga_1:
+                    print("O.C kamu kurang :(")
+            else:
+                ("Stok potion habis")
 
-            
+                
 # Example usage:
 print("<<<SHOP>>>")
 print("\nIrassahaimase! Selamat datang di SHOP!!\n")
@@ -103,11 +125,10 @@ item_Shop = read_csv("item_shop.csv")
 mons = read_csv("monster.csv")
 item_inv = read_csv("item_inventory.csv")
 mons_shop = read_csv("monster_shop.csv")
-mons_inv = read_csv("monster_inventor.csv")
+mons_inv = read_csv("monster_inventory.csv")
 user_id = read_csv("user.csv")
 baca_oc = read_csv("user.csv")
 x = 0
-
 
 pilih_id = int(input("Ini test id nomor berapa: "))
 oc = baca_oc[pilih_id][4]
@@ -116,7 +137,7 @@ while x == 0:
     aksi = str(input("Pilih aksi (lihat/beli/keluar): "))
     if aksi == "keluar":
         print("Mr. Yanto bilang makasih, belanja lagi ya nanti :)")
-        x == 1
+        x += 1
     elif aksi == "lihat":    
         aksi_2 = str(input(f"Mau {aksi} apa (monster/potion): "))
         if aksi_2 == "monster":
@@ -132,7 +153,7 @@ while x == 0:
         if aksi_3 == "monster" or aksi_3 == "Monster" or aksi_3 == "MONSTER":
             aksi_beli_monster = str(input("Masukkan id monster: "))
             beli_mons()
-        if aksi_3 == "potion" or aksi_3 == "Potion" or aksi_3 == "POTION":
-            aksi_beli_potion = str(input("Masukkan id potion: "))
+        elif aksi_3 == "potion" or aksi_3 == "Potion" or aksi_3 == "POTION":
+            aksi_beli_potion = int(input("Masukkan id potion: "))
             jumlah_beli_potion = int(input("Masukkan jumlah potion: "))
             beli_potion()
